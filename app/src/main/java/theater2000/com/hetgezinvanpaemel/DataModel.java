7e2cbccaf14cf8700a2de026e7da271c798ef374
Tom Van de Steene <tom.vandesteene@realdolmen.com>
2018-01-05 10:53:33 +0100

    DataModel.java, MyData.java and cards_layout.xml added + white cardview color added

---
+++ app/src/main/java/theater2000/com/hetgezinvanpaemel/DataModel.java
@@ -0,0 +1,18 @@
+package theater2000.com.hetgezinvanpaemel;
+
+/**
+ * Created by TVDBF57 on 5/01/2018.
+ */
+
+public class DataModel {
+
+    private String fragmentName;
+
+    public DataModel(String fragmentName) {
+        this.fragmentName = fragmentName;
+    }
+
+    public String getFragmentName() {
+        return fragmentName;
+    }
+}
---
+++ app/src/main/java/theater2000/com/hetgezinvanpaemel/MyData.java
@@ -0,0 +1,10 @@
+package theater2000.com.hetgezinvanpaemel;
+
+/**
+ * Created by TVDBF57 on 5/01/2018.
+ */
+
+public class MyData {
+
+    static String[] fragmentNameArray = {"Repetitie Schema", "Text", "Spelers", "Voorstellingen", "Foto Album"};
+}
